dc4bd204cf10f863105d22564887492fe60cec37
Arpit Kr Shrivastava <arpitshrivastava9377@gmail.com>
2025-07-07 16:24:44 +0530

    basics

--- 01basicsreact/src/App.js
+++ 01basicsreact/src/App.js
@@ -1,7 +1,18 @@
+import Chai from './Chai'
+
 function App() {
   return (
+    <>
+    <Chai/>
     <h1>chai aur react | ARPIT KUMAR SHRIVASTAVA </h1>
+    </>
   );
 }
 
 export default App;
+
+// convention 1 whether you work in vite and react youn have to create the component name with capital letter function name in capital 
+// file name may be capitalise 
+
+// agar html return ho rha hai to jsx name rakhte hai varna js 
+// aur vite me jsx 
--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,7 +1,15 @@
+import Chai from "./Chai"
+
 function App() {
   return (
-    <h1>Chai aur react with vite | Arpit Shrivastava</h1>
+    <>
+    <Chai/>
+    <h1>Chai aur react</h1>
+    <p>test para</p>
+    </>
   )
 }
 
 export default App
+
+// jsx me ek hi element return kar sakte hai to ya to div me return ya fir fragement me kar do khlai <> </>
